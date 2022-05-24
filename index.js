281ede80e07fef6e54ef611dd231dde68b007dbc
zannatreya <zannatulbintabahar@yahoo.com>
2022-05-24 15:49:00 +0600

    created product API

--- index.js
+++ index.js
@@ -2,10 +2,41 @@ const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+// 8pB7kH8x9KPL42OT,computerParts
+// computer_parts,product
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.twtll.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('computer_parts').collection('product');
+
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Hello from menufacturer!')
